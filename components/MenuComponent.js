86d7ba33453c5fc503eb34d5040bcbcf39b84dd9
koshi.s <koshi.s@hz-koshis01.nferx.com>
2022-04-05 09:49:27 +0000

    Menu updated News/Blog

---
+++ components/MenuComponent.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import MenuItem from "@material-ui/core/MenuItem";
+import Link from 'next/link'
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import { MenuList } from "nferx-core-ui";
+import { useRouter } from 'next/router'
+
+function MenuComponent() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+  const location = useRouter()
+  const { pathname } = location
+  const splitLocation = pathname.split('/')
+
+  function handleToggle() {
+    setOpen(!open);
+  }
+
+  return (
+    <div
+      onMouseEnter={handleToggle}
+      onMouseLeave={handleToggle}>
+      <Button
+        style={{
+          textDecoration: splitLocation[1] === 'news' || splitLocation[1] === 'blog' ? 'underline' : 'none',
+          textTransform: 'none', fontSize: '16px',
+          fontFamily: "Inter",
+          fontWeight: '400',
+          lineHeight: '16px'
+        }}
+        buttonRef={node => {
+          setAnchorEl(node)
+        }}
+        onClick={handleToggle}
+      >
+        Media
+      </Button>
+
+      <Popper open={open} anchorEl={anchorEl} transition disablePortal>
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom" ? "center top" : "center bottom"
+            }}
+          >
+            <Paper>
+              <MenuList
+                placement="bottom-start"
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                transformOrigin={{
+                  placement: 'bottom-start',
+                  horizontal: 'left',
+                }}>
+                <Link href="/news" ><MenuItem onClick={handleToggle}>News</MenuItem></Link>
+                <Link href="/blog" ><MenuItem onClick={handleToggle}>Blog</MenuItem></Link>
+              </MenuList>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </div>
+  );
+}
+
+export default MenuComponent;
